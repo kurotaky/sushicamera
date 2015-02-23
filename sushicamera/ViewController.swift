1046e87da0b714bad79da131259d84298856838f
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2015-02-24 08:51:30 +0900

    Initial Commit

---
+++ sushicamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sushicamera
+//
+//  Created by usr0600244 on 2015/02/24.
+//  Copyright (c) 2015年 mo-fu.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
